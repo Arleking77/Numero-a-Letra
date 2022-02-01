b84bc671f9d1ce718f9bc08ed833020f67b34830
Arleking77 <pmacaya1234@gmail.com>
2022-02-01 15:11:24 -0300

    convierte numeros a letras

---
+++ src/numerosaletra/Numerosaletra.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package numerosaletra;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author pmaca
+ */
+public class Numerosaletra {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    cod ver = new cod();
+    String numero = JOptionPane.showInputDialog("Ingresar el RUT de la persona"+"\n"+"");
+   
+    }
+    
+}
